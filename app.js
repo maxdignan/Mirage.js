b21ddda625687709e6a8038aaa27d257fb2a9ac4
maxdignan <maxdignan@gmail.com>
2015-10-25 20:00:00 -0400

    template built

--- app.js
+++ app.js
@@ -5,24 +5,21 @@ var mongoose = require('mongoose');
 
 app.use(express.static('./'));
 
-mongoose.connect('mongodb://localhost/db');
+mongoose.connect('mongodb://localhost:27017/db');
 var Schema = mongoose.Schema;
 var todoSchema = new Schema({text: String});
 var todoModel = mongoose.model('testing', todoSchema);
 
 
 //console.log(todoModel);
-
+console.log(app);
 //Here's all you need to do to have a REST api with mongo
-var mirage = require('./mirage.js');
-console.log(mirage); //UNECESSARY LINE
-mirage(app, todoModel, todoModel.modelName, [function(req, res, next){
+var mirage = require('./mirage');
+mirage(app, todoModel, 'api', [function(req, res, next){
 		console.log(req); next();
 }]);
 
-
-
-
+console.log(app);
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
--- mirage.js
+++ mirage.js
@@ -1,17 +1,16 @@
 var express = require('express');
-var mirage = {};
 
-function run(){
+module.exports = function(app, mongo_model, query, middleware){
     var router = express.Router();
 
     //add middleware
-    for (var i = 0; i < mirage.middleware.length; i++) {
-        router.use(mirage.middleware[i]);
+    for (var i = 0; i < middleware.length; i++) {
+        router.use(middleware[i]);
     }
 
     //get all
     router.get('/', function (req, res) {
-        mirage.mongo_model.find({}, function(err, docs){
+        mongo_model.find({}, function(err, docs){
             if (err){
                 res.send('an error occured');
             }
@@ -21,7 +20,7 @@ function run(){
 
     router.get('/:id', function (req, res) {
         var id = req.params.id;
-        mirage.mongo_model.findById(id, function(err, doc){
+        mongo_model.findById(id, function(err, doc){
             if (err){
                 res.send('an error occured');
             }
@@ -30,7 +29,12 @@ function run(){
     });
 
     router.post('/', function (req, res) {
-
+        mongo_model.create(req.body, function(err, doc){
+            if (err){
+                res.send('an error occured');
+            }
+            res.send(doc);
+        });
     });
 
     router.delete('/:id', function(req, res){
@@ -43,16 +47,5 @@ function run(){
 
     });
 
-    mirage.app.use(mirage.query, router);
-}
-
-
-module.exports = function(app, mongo_model, query, middleware){
-	mirage.app = exp_app;
-	mirage.mongo_model = mongo_model;
-	mirage.query = query;
-    mirage.middleware = middleware;
-
-
-    run();
+    app.use(query, router);
 };
